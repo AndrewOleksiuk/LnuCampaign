4666a52fef716eb4c00f21ffd2e4bff7a0c8c849
Andriy Oleksyuk <andre200129@gmail.com>
2021-04-14 16:09:40 +0300

    add stylecop, sonar analyzer solve some errors turn off rules

--- LnuCampaign/LnuCampaign/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ LnuCampaign/LnuCampaign/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Threading.Tasks;
+using LnuCampaign.Core.Data.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
-using LnuCampaign.Core.Data.Entities;
 
 namespace LnuCampaign.Areas.Identity.Pages.Account
 {
